77c675eba944a06caba0782e48843b7cf969efa6
Rusty Abbott <dev@rustyabbott.com>
2023-09-20 15:44:25 +0200

    Add dark mode toggle switch to guest layout

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -19,9 +19,3 @@ createInertiaApp({
         color: '#4B5563',
     },
 })
-
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-} else {
-    document.documentElement.classList.remove('dark')
-};
