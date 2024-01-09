ca3c0e8f2cca2c0f63bce018e0eac3c8c7d65d6b
Rusty Abbott <dev@rustyabbott.com>
2024-01-09 17:58:00 -0500

    Add DarkModeToggle component and make manual toggle functional

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,16 +1,13 @@
+import DarkModeToggle from '@/Components/DarkModeToggle';
 import { Link, Head } from '@inertiajs/react';
 
 export default function Home({ auth, laravelVersion, phpVersion }) {
     return (
         <>
             <Head title="Home" />
-            <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+            <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-white dark:bg-black selection:bg-red-500 selection:text-white">
                 <div className="sm:fixed sm:top-0 sm:left-0 px-4 py-6">
-                    <label className="relative inline-flex items-center cursor-pointer">
-                        <input type="checkbox" value="" className="sr-only peer"/>
-                        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark mode</span>
-                    </label>
+                    <DarkModeToggle></DarkModeToggle>
                 </div>
 
                 <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
