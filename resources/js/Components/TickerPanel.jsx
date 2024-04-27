31ac5408efb77f7adfe729651a0e66629851462e
Rusty Abbott <dev@rustyabbott.com>
2024-04-27 12:45:48 -0400

    TickerPanel styling and money formatting

--- resources/js/Components/TickerPanel.jsx
+++ resources/js/Components/TickerPanel.jsx
@@ -1,8 +1,31 @@
 const TickerPanel = ({ stock }) => {
+    const bgColor = stock.curr_price >= stock.prev_price ? 'bg-green-400' : 'bg-red-700';
+    const priceDiff = stock.curr_price - stock.prev_price;
+    const plusOrMinus = priceDiff >= 0 ? '+' : '-';
+
+    const currentPrice = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    }).format(stock.curr_price);
+
+    const change = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    }).format(priceDiff);
+
     return (
-        <div className="outline-dotted h-[150px]">
-            { stock.stock_name }<br />
-            { stock.curr_price }
+        <div className={`outline-dotted h-[150px] ${bgColor} text-white p-2`}>
+            <div className="text-3xl">
+                { stock.stock_name }
+            </div>
+            <div>
+                { currentPrice }
+            </div>
+            <div>
+                { plusOrMinus + change }
+            </div>
         </div>
     );
 }
